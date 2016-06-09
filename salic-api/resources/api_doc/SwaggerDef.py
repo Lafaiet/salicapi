e979606f956cd3396afeb22aafe898cdeaebaa61
Lafaiet <coldmetall@gmail.com>
2016-06-09 16:10:41 -0300

    Tratamento de erro ao servir definicao swagger

--- salic-api/resources/api_doc/SwaggerDef.py
+++ salic-api/resources/api_doc/SwaggerDef.py
@@ -7,7 +7,16 @@ class SwaggerDef(ResourceBase):
         super (SwaggerDef,self).__init__()
 
     def get(self):
-        swagger_file = open(SWAGGER_DEF_PATH)
-        def_data = open(SWAGGER_DEF_PATH).read()
+        try:
+            swagger_file = open(SWAGGER_DEF_PATH)
+        except Exception:
+            Log.error('error trying to open swagger definition file under the path:  \"%s\"'%SWAGGER_DEF_PATH)
+            result = {'message' : 'internal error',
+                      'message_code' :  13,
+                      'more' : 'something is broken'
+                      }
+            return self.render(result, status_code = 503)
+            
+        def_data = swagger_file.read()
 
         return self.render(def_data)
